0dea0e68b1abf9aeab019c1b9ba2bb2fd9cd2827
Beliavsky <beliavsky@aol.com>
2025-03-31 10:31:30 -0400

    added seq() and other procedures

--- util.f90
+++ util.f90
@@ -5,13 +5,19 @@ implicit none
 private
 public :: default, assert_equal, write_merge, split_string, display, &
    print_time_elapsed, read_words_line, str, print_table, exe_name, &
-   join
+   join, seq, cbind
 interface default
    module procedure default_int, default_real, default_logical, &
       default_character
 end interface default
+interface seq
+   module procedure seq_stride, seq_unit_stride
+end interface seq
+interface cbind
+   module procedure cbind_vec_vec, cbind_mat_vec, cbind_mat_mat
+end interface cbind
 interface display
-   module procedure display_matrix
+   module procedure display_matrix, display_vector
 end interface display
 contains
 
@@ -154,20 +160,37 @@ do
 end do
 end subroutine split_string
 
-subroutine display_matrix(x, outu, fmt_r, fmt_header)
+subroutine display_matrix(x, outu, fmt_r, fmt_header, title)
 ! print a matrix
 real(kind=dp)    , intent(in)           :: x(:,:)
 integer          , intent(in), optional :: outu
-character (len=*), intent(in), optional :: fmt_r, fmt_header
+character (len=*), intent(in), optional :: fmt_r, fmt_header, title
 integer                                 :: i, outu_
 character (len=100)                     :: fmt_r_
 outu_  = default(output_unit, outu)
 fmt_r_ = default("(*(1x,f10.4))", fmt_r)
 if (present(fmt_header)) write(outu_, fmt_header)
+if (present(title)) write (outu_, "(a)") title
 do i=1,size(x,1)
    write(outu_,fmt_r_) x(i,:)
 end do
 end subroutine display_matrix
+
+subroutine display_vector(x, outu, fmt_r, fmt_header, title)
+! print a vector
+real(kind=dp)    , intent(in)           :: x(:)
+integer          , intent(in), optional :: outu
+character (len=*), intent(in), optional :: fmt_r, fmt_header, title
+integer                                 :: i, outu_
+character (len=100)                     :: fmt_r_
+outu_  = default(output_unit, outu)
+fmt_r_ = default("(*(1x,f10.4))", fmt_r)
+if (present(fmt_header)) write(outu_, fmt_header)
+if (present(title)) write (outu_, "(a)") title
+do i=1,size(x)
+   write(outu_,fmt_r_) x(i)
+end do
+end subroutine display_vector
 
 subroutine print_time_elapsed(old_time, outu)
 real(kind=dp), intent(in) :: old_time ! previously set by call cpu_time(old_time)
@@ -255,5 +278,73 @@ do i=2,nw
    str = trim(str) // sep // words(i) 
 end do
 end function join
+
+pure function seq_stride(first, last, stride) result(vec)
+!! return an integer sequence from first through last
+integer, intent(in) :: first, last, stride
+integer, allocatable :: vec(:)
+integer :: i, n, idiff
+idiff = last - first
+n = max(0, 1 + idiff/stride)
+allocate (vec(n))
+do i=1, n
+   vec(i) = first + (i - 1) * stride
+end do
+end function seq_stride
+
+pure function seq_unit_stride(first, last) result(vec)
+!! return an integer sequence from first through last
+integer, intent(in) :: first, last
+integer, allocatable :: vec(:)
+integer :: i, n
+n = max(0, last - first + 1)
+allocate (vec(n))
+do i=1, n
+   vec(i) = first + i - 1
+end do
+end function seq_unit_stride
+
+pure function cbind_vec_vec(x,y) result(xy)
+! return a matrix whose columns are x(:) and y(:)
+real(kind=dp), intent(in) :: x(:), y(:)
+real(kind=dp), allocatable :: xy(:,:)
+integer :: n
+n = size(x,1)
+if (size(y) /= n) error stop "mismatched sizes in cbind"
+xy = reshape([x, y], [n, 2])
+end function cbind_vec_vec
+
+pure function cbind_mat_vec(x,y) result(xy)
+! append vector y(:) to matrix x(:,:)
+real(kind=dp), intent(in) :: x(:,:), y(:)
+real(kind=dp), allocatable :: xy(:,:)
+integer :: n1, n2
+n1 = size(x,1)
+if (size(y) /= n1) error stop "mismatched sizes in cbind"
+n2 = size(x,2)
+allocate (xy(n1, n2+1))
+xy(:,:n2)  = x
+xy(:,n2+1) = y 
+end function cbind_mat_vec
+
+pure function cbind_mat_mat(x,y) result(xy)
+! append columns of y(:,:) to matrix x(:,:)
+real(kind=dp), intent(in) :: x(:,:), y(:,:)
+real(kind=dp), allocatable :: xy(:,:)
+integer :: n1, n2
+n1 = size(x,1)
+if (size(y,1) /= n1) error stop "mismatched sizes in cbind"
+n2 = size(x,2)
+allocate (xy(n1, n2+size(y,2)))
+xy(:,:n2)  = x
+xy(:,n2+1:) = y 
+end function cbind_mat_mat
+
+! function appended_char_vec(x, y) result(xy)
+! character (len=*), intent(in) :: x(:)
+! character (len=*), intent(in) :: y
+! character (len=len(x)), allocatable :: xy(:)
+!  
+! end function appended_char_vec
 
 end module util_mod
