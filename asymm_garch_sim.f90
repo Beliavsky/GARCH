7ca809940ddf8a78cd5b8ce116c912de1c01f4e4
Beliavsky <beliavsky@aol.com>
2025-03-31 13:45:11 -0400

    simulate asymmetric GARCH processes

---
+++ asymm_garch_sim.f90
@@ -0,0 +1,61 @@
+module asymm_garch_sim_mod
+use        kind_mod, only: dp
+! use optim_methods_mod, only: nelder_mead_str, uobyqa_str
+! use nelder_mead_mod, only: nelder_mead
+! use     obj_fun_mod, only: xret, xdist, gjr_garch_sigma, obj_sub
+! use basic_stats_mod, only: variance
+! use      random_mod, only: random_normal
+! use   constants_mod, only: pi
+use        util_mod, only: assert_equal, default
+! use      uobyqa_mod, only: uobyqa
+implicit none
+private
+public :: simulate_shift_twist_garch
+integer, parameter :: nparam_gjr_garch = 5
+contains
+
+subroutine simulate_shift_twist_garch(z, ret, mu, omega, alpha, &
+   gamma, beta, c_shift, sigma, iprint)
+! simulate an asymmetric GARCH(1,1) process with shift and twist parameters
+!
+! Input:
+! z     -- standardized noise 
+! mu    -- mean return
+! omega -- variance constant
+! alpha -- symmetric weight on past squared return
+! gamma -- weight on past squared return when return is negative
+! beta  -- weight on previous variance
+! c_shift -- shift of standardized noise in variance equation
+! iprint
+!
+! Output:
+! ret -- Simulated return series
+! sigma (optional) -- conditional standard deviation
+real(kind=dp), intent(in)  :: mu, omega, alpha, gamma, beta, c_shift
+real(kind=dp), intent(in)  :: z(:) ! noise with 0 mean and unit variance
+real(kind=dp), intent(out) :: ret(:)
+real(kind=dp), intent(out), optional :: sigma(:)
+integer, intent(in), optional :: iprint
+real(kind=dp) :: uc_var, sigma2, noise, noise_shift
+integer :: i, iprint_, n
+real(kind=dp) :: sigma_i
+iprint_ = default(0, iprint)
+n = size(ret)
+if (present(sigma)) call assert_equal(size(sigma), n, &
+   "in simulate_shift_twist_garch, size(sigma)")
+uc_var = omega / (1.0_dp - (1.0_dp + c_shift**2) * (alpha + 0.5_dp*gamma) - &
+   beta)
+if (iprint_ > 0) print*,"uc_var:", uc_var
+sigma2 = uc_var
+do i = 1, n
+   sigma_i = sqrt(sigma2)
+   noise = sigma_i * z(i)
+   noise_shift = sigma_i * (z(i) - c_shift)
+   if (present(sigma)) sigma(i) = sigma_i
+   ret(i) = mu + noise
+   if (iprint_ > 1) print "(i8, *(f10.6))", i,sigma_i, z(i), noise, ret(i)
+   sigma2 = (alpha + merge(gamma,0.0_dp,noise_shift < 0.0_dp)) * &
+              noise_shift**2 + beta*sigma2 + omega
+end do
+end subroutine simulate_shift_twist_garch
+end module asymm_garch_sim_mod
