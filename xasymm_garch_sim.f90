f2c448d9bbb0873c69f56e1e888e4378f3e2e104
Beliavsky <beliavsky@aol.com>
2025-03-31 17:26:10 -0400

    printed parameters used, simulated many time series (set by niter)

--- xasymm_garch_sim.f90
+++ xasymm_garch_sim.f90
@@ -6,7 +6,8 @@ use asymm_garch_sim_mod, only: simulate_shift_twist_garch
 use random_mod         , only: random_normal
 use basic_stats_mod    , only: print_basic_stats
 implicit none
-integer, parameter :: n = 10**4
+integer :: iter
+integer, parameter :: n = 10**6, niter = 20
 real(kind=dp) :: z(n), ret(n), sigma(n), mu, omega, alpha, beta, gamma, &
    c_shift
 mu = 0.0_dp
@@ -15,10 +16,16 @@ beta = 0.7_dp
 alpha = 0.1_dp
 gamma = 0.3_dp
 c_shift = 0.0_dp
-z = random_normal(n)
-call simulate_shift_twist_garch(z, ret, mu, omega, alpha, gamma, beta, &
-   c_shift, sigma)
-call print_basic_stats(reshape([z, ret, sigma], [n, 3]), &
-   labels=[character (len=5) :: "z", "ret", "sigma"], &
-   fmt_cr="(a10,*(f10.4))")
+print "(*(a10))",  "#obs", "mu", "omega", "beta", "alpha", "gamma", "c_shift"
+print "(i10,*(f10.4))", n,  mu ,  omega ,  beta ,  alpha ,  gamma ,  c_shift 
+print*
+do iter=1,niter
+   z = random_normal(n)
+   call simulate_shift_twist_garch(z, ret, mu, omega, alpha, gamma, beta, &
+      c_shift, sigma)
+   call print_basic_stats(reshape([z, ret, sigma], [n, 3]), &
+      labels=[character (len=5) :: "z", "ret", "sigma"], &
+      fmt_cr="(a10,*(f10.4))")
+   print "('first, last sigma:',2(1x,f10.4), /)", sigma([1, n])
+end do
 end program xasymm_garch_sim
