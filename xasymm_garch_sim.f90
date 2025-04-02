96e8ba1874b73a56332bae4dbe4397792ede8c19
Beliavsky <beliavsky@aol.com>
2025-04-02 15:37:40 -0400

    stores simulatd data in xmat

--- xasymm_garch_sim.f90
+++ xasymm_garch_sim.f90
@@ -9,7 +9,7 @@ implicit none
 integer :: iter
 integer, parameter :: n = 10**6, niter = 20
 real(kind=dp) :: z(n), ret(n), sigma(n), mu, omega, alpha, beta, gamma, &
-   c_shift
+   c_shift, xmat(n, 3)
 mu = 0.0_dp
 omega = 0.1_dp
 beta = 0.7_dp
@@ -23,8 +23,8 @@ do iter=1,niter
    z = random_normal(n)
    call simulate_shift_twist_garch(z, ret, mu, omega, alpha, gamma, beta, &
       c_shift, sigma)
-   call print_basic_stats(reshape([z, ret, sigma], [n, 3]), &
-      labels=[character (len=5) :: "z", "ret", "sigma"], &
+   xmat = reshape([z, ret, sigma], [n, 3])
+   call print_basic_stats(xmat, labels=[character (len=5) :: "z", "ret", "sigma"], &
       fmt_cr="(a10,*(f10.4))")
    print "('first, last sigma:',2(1x,f10.4), /)", sigma([1, n])
 end do
