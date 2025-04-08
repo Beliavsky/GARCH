7a4acfc53fc661d96ed2c7e689677e624e19c13d
Beliavsky <beliavsky@aol.com>
2025-04-08 15:37:07 -0400

    Add ACF calculation and add columns of |ret| and ret^2

--- xasymm_garch_sim.f90
+++ xasymm_garch_sim.f90
@@ -4,12 +4,15 @@ program xasymm_garch_sim
 use kind_mod           , only: dp
 use asymm_garch_sim_mod, only: simulate_shift_twist_garch
 use random_mod         , only: random_normal
-use basic_stats_mod    , only: print_basic_stats
+use basic_stats_mod    , only: print_basic_stats, print_acf
 implicit none
 integer :: iter
-integer, parameter :: n = 10**6, niter = 20
+integer, parameter :: n = 10**6, niter = 1, ncol = 5
 real(kind=dp) :: z(n), ret(n), sigma(n), mu, omega, alpha, beta, gamma, &
-   c_shift, xmat(n, 3)
+   c_shift, xmat(n, ncol)
+character (len=*), parameter :: labels(ncol) = [character (len=5) :: &
+   "z", "ret", "|ret|", "ret^2", "sigma"]
+! character (len=:), allocatable :: labels(:)
 mu = 0.0_dp
 omega = 0.1_dp
 beta = 0.7_dp
@@ -23,9 +26,9 @@ do iter=1,niter
    z = random_normal(n)
    call simulate_shift_twist_garch(z, ret, mu, omega, alpha, gamma, beta, &
       c_shift, sigma)
-   xmat = reshape([z, ret, sigma], [n, 3])
-   call print_basic_stats(xmat, labels=[character (len=5) :: "z", "ret", "sigma"], &
-      fmt_cr="(a10,*(f10.4))")
-   print "('first, last sigma:',2(1x,f10.4), /)", sigma([1, n])
+   xmat = reshape([z, ret, abs(ret), ret**2, sigma], [n, ncol])
+   call print_basic_stats(xmat, labels=labels, fmt_cr="(a10,*(f10.4))")
+   call print_acf(xmat, nacf=5, labels=labels, title="")
+   print "(/,'first, last sigma:',2(1x,f10.4), /)", sigma([1, n])
 end do
 end program xasymm_garch_sim
