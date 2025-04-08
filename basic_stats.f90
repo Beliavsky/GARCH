db3e5ad6b24807fedaf8f1a07d2fe0ae5c8daf11
Beliavsky <beliavsky@aol.com>
2025-04-08 15:21:28 -0400

    Add covariance and autocorrelation functions and improved printing

--- basic_stats.f90
+++ basic_stats.f90
@@ -7,7 +7,8 @@ private
 public :: mean, variance, sd, mean_and_sd, kurtosis, basic_stats, &
    print_basic_stats, basic_stats_names, correl, acf, nbasic_stats, &
    stat, stats, corr_mat, rms, moving_sum, moving_average, &
-   print_corr_mat, skew
+   print_corr_mat, skew, cov, cov_mat, print_cov_mat, print_acf_mat, &
+   print_acf
 integer, parameter :: nbasic_stats = 6
 character (len=*), parameter :: basic_stats_names(nbasic_stats) = &
    [character(len=4) :: "mean", "sd", "skew", "kurt", "min", "max"]
@@ -15,9 +16,15 @@ real(kind=dp), parameter :: bad_value = -huge(1.0d0)
 interface stats
    module procedure stats_many_vec, stats_many_mat
 end interface stats
+interface print_acf
+   module procedure print_acf_vec, print_acf_mat
+end interface print_acf   
 interface print_basic_stats
    module procedure print_basic_stats_vec, print_basic_stats_mat
 end interface print_basic_stats
+interface acf
+   module procedure acf_vec, acf_mat
+end interface acf
 contains
 
 function stats_many_vec(funcs, x) result(y)
@@ -202,6 +209,7 @@ write (outu_, fmt_stats_names_) "", (trim(basic_stats_names(i)), i=1,nbasic_stat
 do i=1,size(x, 2)
    write (outu_, fmt_cr_) trim(labels(i)), basic_stats(x(:,i))
 end do
+if (present(fmt_trailer)) write (outu_, fmt_trailer)
 end subroutine print_basic_stats_mat
 
 pure function correl(x, y) result(corr_xy)
@@ -228,7 +236,22 @@ else
 end if
 end function correl
 
-pure function acf(x, nacf) result(xacf)
+pure function cov(x, y) result(cov_xy)
+! Returns the covariance of two 1D arrays
+real(kind=dp), intent(in) :: x(:), y(:)
+real(kind=dp) :: cov_xy
+real(kind=dp) :: x_mean, y_mean
+integer :: n
+n = size(x)
+if (n /= size(y) .or. n == 0) then
+   error stop "x and y must have same size > 0 in cov"
+end if
+x_mean = sum(x) / n
+y_mean = sum(y) / n
+cov_xy = sum((x - x_mean) * (y - y_mean))
+end function cov
+
+pure function acf_vec(x, nacf) result(xacf)
 ! return the autocorrelations at lags 1 through nacf
 real(kind=dp), intent(in) :: x(:)         ! Input array
 integer, intent(in) :: nacf               ! Number of autocorrelations to compute
@@ -243,7 +266,69 @@ denom = sum(xdm**2)
 do lag = 1, nacf
    xacf(lag) = sum(xdm(1:n-lag) * xdm(lag+1:n)) / denom
 end do
-end function acf
+end function acf_vec
+
+pure function acf_mat(x, nacf) result(xacf)
+! return the autocorrelations at lags 1 through nacf
+real(kind=dp), intent(in) :: x(:,:)       ! Input array
+integer, intent(in) :: nacf               ! Number of autocorrelations to compute
+real(kind=dp) :: xacf(nacf,size(x,2))     ! Output array for autocorrelations
+integer :: icol
+do icol=1,size(x,2)
+   xacf(:,icol) = acf(x(:,icol), nacf)
+end do
+end function acf_mat
+
+subroutine print_acf_vec(x, nacf, label, outu, fmt_header, &
+   fmt_trailer, title, fmt_acf, fmt_label)
+! print the autocorrelations at lags 1 through nacf of x(:)
+real(kind=dp), intent(in) :: x(:)       ! Input array
+integer, intent(in) :: nacf             ! Number of autocorrelations to compute
+character (len=*), intent(in), optional :: title, label, &
+   fmt_header, fmt_trailer, fmt_acf, fmt_label
+character (len=100) :: fmt_acf_, fmt_label_
+integer, intent(in), optional :: outu
+real(kind=dp) :: xacf(nacf)     
+integer :: iacf, outu_
+outu_ = default(output_unit, outu)
+fmt_label_ = default("(6x,a8)", fmt_label)
+if (present(fmt_header)) write (outu_, fmt_header)
+if (present(title)) write (outu_, "(a)") title
+if (present(label)) write (outu_,fmt_label_) label
+fmt_acf_ = default("('ACF_', i2.2, f8.4)", fmt_acf)
+xacf = acf_vec(x, nacf)
+do iacf=1,nacf
+   write (outu_, fmt_acf_) iacf, xacf(iacf)
+end do
+if (present(fmt_trailer)) write (outu_, fmt_trailer)
+end subroutine print_acf_vec
+
+subroutine print_acf_mat(x, nacf, labels, outu, fmt_header, &
+   fmt_trailer, title, fmt_acf, fmt_labels)
+! print the autocorrelations at lags 1 t hrough nacf of the columns of x(:,:)
+real(kind=dp), intent(in) :: x(:,:)       ! Input array
+integer, intent(in) :: nacf               ! Number of autocorrelations to compute
+character (len=*), intent(in), optional :: title, labels(:), &
+   fmt_header, fmt_trailer, fmt_acf, fmt_labels
+integer, intent(in), optional :: outu
+real(kind=dp) :: xacf(nacf,size(x,2))
+integer :: iacf, icol, outu_
+character (len=100) :: fmt_acf_, fmt_labels_
+outu_ = default(output_unit, outu)
+fmt_labels_ = default("(6x,*(a8))", fmt_labels)
+if (present(fmt_header)) then
+   write (outu_, fmt_header)
+end if
+if (present(title)) write (outu_, "(a)") title
+if (present(labels)) write (outu_, fmt_labels_) &
+   (trim(labels(icol)), icol=1,size(labels))
+xacf = acf_mat(x, nacf)
+fmt_acf_ = default("('ACF_', i2.2, *(f8.4))", fmt_acf)
+do iacf=1,nacf
+   write (outu_, fmt_acf_) iacf, xacf(iacf,:)
+end do
+if (present(fmt_trailer)) write (outu_, fmt_trailer)
+end subroutine print_acf_mat
 
 subroutine print_corr_mat(x, col_names, outu, fmt_col_names, fmt_row, &
    fmt_header, fmt_trailer)
@@ -260,6 +345,22 @@ call print_table(corr_mat(x), row_names=col_names, col_names=col_names, &
    fmt_header=fmt_header, fmt_trailer=fmt_trailer, outu=outu, &
    fmt_col_names=fmt_col_names_, fmt_row=fmt_row_)
 end subroutine print_corr_mat
+
+subroutine print_cov_mat(x, col_names, outu, fmt_col_names, fmt_row, &
+   fmt_header, fmt_trailer)
+! print the covariance matrix of the columns of x(:,:)
+real(kind=dp), intent(in) :: x(:,:)
+character (len=*), intent(in) :: col_names(:)
+integer          , intent(in), optional :: outu ! output unit
+character (len=*), intent(in), optional :: fmt_header, fmt_trailer, &
+   fmt_col_names, fmt_row
+character (len=100) :: fmt_col_names_, fmt_row_
+fmt_col_names_ = default("(*(a8,:,1x))", fmt_col_names)
+fmt_row_ = default("(a8, *(1x,f8.4))", fmt_row)
+call print_table(cov_mat(x), row_names=col_names, col_names=col_names, &
+   fmt_header=fmt_header, fmt_trailer=fmt_trailer, outu=outu, &
+   fmt_col_names=fmt_col_names_, fmt_row=fmt_row_)
+end subroutine print_cov_mat
 
 pure function corr_mat(x) result(cor)
     ! return the correlation matrix of the columns of x(:,:)
@@ -285,6 +386,28 @@ pure function corr_mat(x) result(cor)
     cor = cor / spread(std_vec, dim=1, ncopies=p)
     cor = cor / spread(std_vec, dim=2, ncopies=p)
 end function corr_mat
+
+pure function cov_mat(x) result(xcov)
+    ! return the covariance matrix of the columns of x(:,:)
+    real(kind=dp), intent(in) :: x(:,:)
+    real(kind=dp)             :: xcov(size(x,2), size(x,2))
+    real(kind=dp)             :: mean_vec(size(x,2)), std_vec(size(x,2))
+    real(kind=dp)             :: centered_x(size(x,1), size(x,2))
+    integer                   :: n, p
+
+    n = size(x, 1)  ! Number of rows
+    p = size(x, 2)  ! Number of columns
+
+    ! Compute the mean of each column
+    mean_vec = sum(x, dim=1) / n
+
+    ! Center the matrix by subtracting the mean of each column
+    centered_x = x - spread(mean_vec, dim=1, ncopies=n)
+
+    ! Compute the standard deviation of each column
+    std_vec = sqrt(sum(centered_x**2, dim=1) / (n - 1))
+    xcov = matmul(transpose(centered_x), centered_x) / (n - 1)
+end function cov_mat
 
 pure function moving_sum(x, k) result(xsum)
 ! return a moving sum of x(:) with k terms, using fewer terms for i < k
